262afdf4267377911b9d2aface48409fd5e12ee8
Clark Moody <clark@clarkmoody.com>
2021-05-15 14:07:16 -0500

    Anti-aliasing based on random samples per pixel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::distributions::{Distribution, Uniform};
+
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::Path;
@@ -28,6 +30,11 @@ fn main() {
     let aspect_ratio = (16, 9);
     let get_width = |height: usize| aspect_ratio.0 * height / aspect_ratio.1;
     let width = get_width(height);
+    let samples_per_pixel = 100;
+
+    // Random number utilities
+    let udist = Uniform::new(0.0, 1.0);
+    let mut rng = rand::thread_rng();
 
     let mut image_data = vec![0; width * height * 3];
 
@@ -43,18 +50,22 @@ fn main() {
     let camera = Camera::new(2.0, 16.0 / 9.0, 1.0);
 
     for y in 0..height {
-        let v = 1.0 - y as f64 / (height - 1) as f64;
-
         print!("\rScanlines remaining {:>5}", height - y);
         std::io::stdout().flush().unwrap();
-
         for x in 0..width {
-            let u = x as f64 / (width - 1) as f64;
+            let mut color = Color::ZERO;
+            for _ in 0..samples_per_pixel {
+                let rand_x = udist.sample(&mut rng);
+                let rand_y = udist.sample(&mut rng);
+                let u = (x as f64 + rand_x) / (width - 1) as f64;
+                let v = 1.0 - (y as f64 + rand_y) / (height - 1) as f64;
 
-            let r = camera.get_ray(u, v);
-            let color = ray_color(&r, &world);
+                let r = camera.get_ray(u, v);
+                color += ray_color(&r, &world);
+            }
+            color /= samples_per_pixel as f64;
+
             let rgb: [u8; 3] = color.as_color_u8();
-
             let i = image_index(x, y);
             image_data[i] = rgb[0];
             image_data[i + 1] = rgb[1];
@@ -63,7 +74,7 @@ fn main() {
     }
     print!("\r");
 
-    let path = Path::new(r"./output/hittables.png");
+    let path = Path::new(r"./output/antialiasing.png");
     let file = File::create(path).unwrap();
     let w = &mut BufWriter::new(file);
 
